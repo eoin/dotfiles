a4bd9c15350990602ed8f6df244b765649dbae66
Eoin Hennessy <eoin.hennessy@gmail.com>
2012-10-30 09:44:48 -0700

    Update color-theme.el

--- emacs.d/elisp/theme.el
+++ emacs.d/elisp/theme.el
@@ -1,6 +1,4 @@
 ;; theme
 (require 'color-theme)
-
-(load-file "~/.emacs.d/vendor/twilight-emacs/color-theme-twilight.el")
+(load-file "~/.emacs.d/vendor/themes/twilight-emacs/color-theme-twilight.el")
 (color-theme-twilight)
-
